e5406be550bf23653e3b2b828d03c32df0f12378
Marcio Ferreira <profhaeliomarcio@gmail.com>
2023-11-18 10:29:44 -0300

    Projeto api

---
+++ src/Healthcare.Api/Controllers/PatientController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Healthcare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        // GET: api/<PatientController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/<PatientController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            return Ok("value");
+        }
+
+        // POST api/<PatientController>
+        [HttpPost]
+        public IActionResult Post(ProblemDetails problemDetails)
+        {
+            return CreatedAtAction("api/Patient/", problemDetails);
+        }
+
+        // PUT api/<PatientController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/<PatientController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NoContent();
+        }
+    }
+}
